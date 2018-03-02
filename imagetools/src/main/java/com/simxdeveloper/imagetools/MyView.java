661d74ccc8a25dcdd264bc61bd2817c80ade215c
Bambang Heri Setiawan <setiawan.heri.bambang@gmail.com>
2018-03-03 03:02:40 +0700

    init

---
+++ imagetools/src/main/java/com/simxdeveloper/imagetools/MyView.java
@@ -0,0 +1,23 @@
+package com.simxdeveloper.imagetools;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by simx on 03/03/18.
+ */
+
+public class MyView extends LinearLayout {
+
+    public MyView(Context context) {
+        super(context);
+
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
